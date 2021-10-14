8cd630cec46d5f5cd8c5ba2d63c4c3297d434297
Joey R <rehmanjoey@gmail.com>
2021-10-14 20:36:12 +0100

    First / commands

--- index.js
+++ index.js
@@ -11,14 +11,77 @@ const client = new DiscordJS.Client({
 
 client.on('ready', () => {
     console.log('Bot is online.');
+
+    const guildId = '898229423336218645';
+    const guild = client.guilds.cache.get(guildId);
+    let commands 
+
+    if (guild) {
+        commands = guild.commands;
+    } else {
+        commands = client.application?.commands;
+    };
+
+    commands?.create({
+        name: 'ping',
+        description: 'Replies with pong.',
+    })
+
+    commands?.create({
+        name: 'add',
+        description: 'Adds two numbers.',
+        options: [
+            {
+                name: 'num1',
+                description: 'The first number.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER
+            },
+            {
+                name: 'num2',
+                description: 'The second number.',
+                required: true,
+                type: DiscordJS.Constants.ApplicationCommandOptionTypes.NUMBER,
+            },
+        ],
+    });
 });
 
+    
 client.on('messageCreate', (message) => {
     if (message.content === 'ping') {
         message.reply({
             content: 'pong',
-        })
-    }
+        });
+    };
 });
+
+client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    } 
+
+    const { commandName, options } = interaction;
+
+    if (commandName === 'ping') {
+        interaction.reply({
+            content: 'Pong!',
+            ephemeral: true,
+        });
+    } else if (commandName === 'add') {
+        const num1 = options.getNumber('num1');
+        const num2 = options.getNumber('num2');
+
+        await interaction.deferReply({
+            ephemeral: true,
+        });
+
+        await interaction.editReply({
+            content: `The sum is ${num1 + num2}`,
+        });
+    };
+});
+
+
 
 client.login(process.env.TOKEN);

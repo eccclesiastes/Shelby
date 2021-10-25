20b45223d9a43f3506c5c08c1542c1a4e64bb2b8
Joey R <rehmanjoey@gmail.com>
2021-10-25 23:13:56 +0100

    About command converted

--- index.js
+++ index.js
@@ -32,11 +32,6 @@ client.on('ready', () => {
     };
 
     commands?.create({
-        name: 'about',
-        description: 'About this bot.',
-    })
-
-    commands?.create({
         name: 'unmute',
         description: 'Unmute a user.',
         options: [
@@ -176,27 +171,7 @@ client.on('interactionCreate', async (interaction) => {
 
     const { commandName, options } = interaction;
 
-    if (commandName === 'about') {
-        const embed = new DiscordJS.MessageEmbed()
-                .setColor('#2f3136')
-                .setAuthor('About Shelby', ``) //link
-                .setDescription('Shelby is a **moderation dedicated** bot which uses purely **ephemeral slash commands** to provide a completely **"invisible"** experience to normal users, while still being of great use to moderators. Start your **better moderation experience** by interacting with the buttons below to **invite** me.')
-                .setFooter('Made with <3 by dceu#0001')
-
-        const linkRow = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setURL('https://discord.com/api/oauth2/authorize?client_id=898229527761788990&permissions=277495671895&scope=applications.commands%20bot')
-                    .setLabel('Invite me!')
-                    .setStyle('LINK')
-            )
-
-        interaction.reply({
-            embeds: [embed],
-            ephemeral: true,
-            components: [linkRow],
-        });
-    } else if (commandName === 'unmute') {
+    if (commandName === 'unmute') {
         const memberTarger = options.getMember('user');
         const reasonTarger = options.getString('reason') || 'No reason provided.';
         const muteRole = interaction.guild.roles.cache.find(role => role.name == 'Muted');

8aa6da2386b70f5bda30e394cf03d0d989c39c6e
Joey R <rehmanjoey@gmail.com>
2021-10-26 12:35:21 +0100

    Userinfo command converted

---
+++ commands/userinfo.js
@@ -0,0 +1,36 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('userinfo')
+        .setDescription('Shows information on a user.')
+        .addUserOption(option =>
+            option.setName('user')
+                    .setDescription('The user to find information on.')
+                    .setRequired(false)),
+    async execute(interaction) {
+        try {
+            const memberTarger = options.getMember('user') || interaction.member;
+            const user = client.users.fetch(memberTarger);
+            const pfp = memberTarger.displayAvatarURL();
+    
+            const embed = new DiscordJS.MessageEmbed()
+                    .setThumbnail(pfp)
+                    .setAuthor(`${(await user).tag} / ${(await user).id}`, `${pfp}`)
+                    .setColor('#2f3136')
+                    .addField(`Created at`, `\n <t:${Math.round((await user).createdAt / 1000)}:D>`, true)
+                    .addField(`Joined at`, `\n <t:${Math.round(memberTarger.joinedTimestamp / 1000)}:D>`, true)
+                    .addField('Roles', `\n ${memberTarger.roles.cache.map(r => r.toString()).join(" ")}`, true)
+    
+            interaction.reply({
+                embeds: [embed],
+                ephemeral: true,
+            });
+        } catch (err) {
+            interaction.reply({
+                content: `Unknown error, please re-try.`,
+                ephemeral: true,
+            });
+        };
+    },
+};
--- index.js
+++ index.js
@@ -32,19 +32,6 @@ client.on('ready', () => {
     };
 
     commands?.create({
-        name: 'userinfo',
-        description: 'Shows information on a user.',
-        options: [
-            {
-                name: 'user',
-                description: 'The user to find information on.',
-                required: false,
-                type: DiscordJS.Constants.ApplicationCommandOptionTypes.USER
-            },
-        ],
-    })
-
-    commands?.create({
         name: 'role',
         description: 'Adds or removes a role to a user.',
         options: [
@@ -133,31 +120,7 @@ client.on('interactionCreate', async (interaction) => {
 
     const { commandName, options } = interaction;
 
-    if (commandName === 'userinfo') {
-        try {
-        const memberTarger = options.getMember('user') || interaction.member;
-        const user = client.users.fetch(memberTarger);
-        const pfp = memberTarger.displayAvatarURL();
-
-        const embed = new DiscordJS.MessageEmbed()
-                .setThumbnail(pfp)
-                .setAuthor(`${(await user).tag} / ${(await user).id}`, `${pfp}`)
-                .setColor('#2f3136')
-                .addField(`Created at`, `\n <t:${Math.round((await user).createdAt / 1000)}:D>`, true)
-                .addField(`Joined at`, `\n <t:${Math.round(memberTarger.joinedTimestamp / 1000)}:D>`, true)
-                .addField('Roles', `\n ${memberTarger.roles.cache.map(r => r.toString()).join(" ")}`, true)
-
-        interaction.reply({
-            embeds: [embed],
-            ephemeral: true,
-        });
-    } catch (err) {
-        interaction.reply({
-            content: `Unknown error, please re-try.`,
-            ephemeral: true,
-        });
-    };
-    } else if (commandName === 'role') {
+    if (commandName === 'role') {
         try {
         const memberTarger = options.getMember('user');
         const roleTarger = options.getRole('role');

c3c5764756f50453ff94fbef3432edbcff12ad8a
Joey R <rehmanjoey@gmail.com>
2021-10-30 22:05:34 +0100

    Updated all commands (node deploy-commands.js)

--- deploy-commands.js
+++ deploy-commands.js
@@ -16,3 +16,18 @@ for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
 	commands.push(command.data.toJSON());
 }
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();

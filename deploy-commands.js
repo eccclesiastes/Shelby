3dfbe4adcfd9596cc1d6a8782709137984d3273f
Joey R <rehmanjoey@gmail.com>
2021-11-01 21:21:22 +0000

    Using global commands now

--- deploy-commands.js
+++ deploy-commands.js
@@ -22,7 +22,7 @@ for (const file of commandFiles) {
 		console.log('Started refreshing application (/) commands.');
 
 		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
+			Routes.applicationCommands(clientId),
 			{ body: commands },
 		);
 

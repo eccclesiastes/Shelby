c1c9fe6549849a7512e1e36ac207b7e2c6b4f46f
Joey R <rehmanjoey@gmail.com>
2021-11-01 20:53:24 +0000

    All connects throught databaseConfig

--- databaseConfig.js
+++ databaseConfig.js
@@ -9,7 +9,7 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) { throw err; };
-    console.log("Connected!");
+    console.log("Connected from databaseConfig.js!");
 });
 
 module.exports = {

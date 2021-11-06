214b5c3f5129218f22a9055d78734afe6da48fc9
Joey R <rehmanjoey@gmail.com>
2021-11-06 22:53:45 +0000

    Fixed mute timeless logging

--- commands/mute.js
+++ commands/mute.js
@@ -86,7 +86,38 @@ module.exports = {
 
         if (!timeTarger) {
             await memberTarger.roles.add(muteRole.id);
+
             memberTarger.send({ embeds: [actionTaken] });
+
+            connection.execute(`SELECT log_channel_id FROM configuration WHERE guild_id=?`, [guildID], function (err, result) {
+                if (err) { throw err; };
+                console.log(result);
+        
+                if(result == null) { 
+                const logReject = new DiscordJS.MessageEmbed()
+                        .setColor('#2f3136')
+                        .setTitle('Unable to log action')
+                        .setDescription(`❌ **| Action cannot be logged as there has been no logging channel found. |** ❌`)
+    
+                    interaction.followUp({
+                        embeds: [logReject],
+                        ephemeral: true
+                    });
+                } else {  
+                    const logEmbed = new DiscordJS.MessageEmbed()
+                        .setColor('#2f3136')
+                        .setAuthor(`❌ ${memberTarger.user.tag} was muted`, `${pfp}`)
+                        .addField(`Invoker`, `${interaction.member} / \`${interaction.member.tag}\``, true)
+                        .addField(`Target`, `${memberTarger} / \`${memberTarger.id}\``, true)
+                        .addField(`Reason`, `${reasonTarger}`, true)
+                        .setTimestamp()
+            
+            
+                    const channel = client.channels.cache.get(result[0].log_channel_id.toString());
+                    channel.send({embeds:[logEmbed]});
+                };
+            });
+
             interaction.editReply({
                 embeds: [embed],
                 ephemeral: true,

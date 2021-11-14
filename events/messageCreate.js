718bf9ed806205a88ac143cffafead69744197a5
Joey R <rehmanjoey@gmail.com>
2021-11-14 13:44:56 +0000

    Now responds on @ping

---
+++ events/messageCreate.js
@@ -0,0 +1,37 @@
+const DiscordJS = require('discord.js');
+const { MessageActionRow, MessageButton } = require('discord.js');
+const join = '898587285111603221';
+
+module.exports = {
+	name: 'messageCreate',
+	async execute(message, client) {
+        if (message.mentions.users.has('898229527761788990')) {
+
+    const embed = new DiscordJS.MessageEmbed()
+        .setAuthor("Hey, I'm Shelby", 'https://cdn.discordapp.com/avatars/898229527761788990/9045f776607eee7e0bfea538434ea8af.webp')
+        .setColor('#2f3136')
+        .setDescription(`I'm a moderation dedicated bot. My prefix will always be \`/\` \n \n For a full list of my commands, please use \`/help\``)
+        .setFooter('Made with <3 by dceu#0001')
+
+        const linkRow = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setURL('https://discord.com/api/oauth2/authorize?client_id=898229527761788990&permissions=277495671895&scope=applications.commands%20bot')
+                    .setLabel('Invite me!')
+                    .setStyle('LINK')
+            )
+            .addComponents(
+                new MessageButton()
+                    .setURL('https://discord.com') //link
+                    .setLabel('Vote for me!')
+                    .setStyle('LINK')
+            )
+
+        message.reply({ 
+            embeds: [embed],
+            components: [linkRow],
+            allowedMentions: { users: [] },
+            });
+        }
+	},
+};
